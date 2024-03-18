d935f92db3670fcff6372c8aa550696ba277d65f
dishavanani13 <dishvanani13@gmail.com>
2024-03-17 21:25:57 -0400

    icon Update

--- Go Tips Split/Controller/LaunchScreenViewController.swift
+++ Go Tips Split/Controller/LaunchScreenViewController.swift
@@ -12,7 +12,7 @@ class LaunchScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
